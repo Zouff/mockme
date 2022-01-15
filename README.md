95ec79378020218d1bcc857ccda5179b4f59b4ed
Blaise Gaillard <bgaillard@wbcoordinator.com>
2022-01-15 12:46:54 -0500

    Dynamic endpoints by adding json files

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Super basic mocked backend
 ## Setup and requirements
 
 1. Install Docker
-2. Modify the `mock.json` file to the result you want to receive
+2. Add a `.json` file to the `mocks` directory for your desired data and endpoint, it will create an endpoint
 3. Run `docker build . -t mockme`
 4. Run `docker run -p 8080:8080 mockme`
 5. Navigate to [this URL](http://localhost:8080) to test that it is returning what you expect
--- server.js
+++ server.js
@@ -2,17 +2,23 @@
 
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const MOCKSDIRECTORY = 'mocks';
 
 // App
 const app = express();
-let mockData = fs.readFileSync('mock.json');
+fs.readdir(MOCKSDIRECTORY, (err, files) => {
+  files.forEach(file => {
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/' + file);
 
-app.get('/', (req, res) => {
-  res.json(JSON.parse(mockData))
+    app.get('/' + path.parse(file).name, (req, res) => {
+      res.json(JSON.parse(mockData))
+    });
+  });
 });
 
 app.listen(PORT, HOST);
