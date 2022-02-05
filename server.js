8ac6334d3c9866b076baa267638e5da486fa638d
Blaise Gaillard <bgaillard@wbcoordinator.com>
2022-02-05 18:46:08 -0500

    More verbs

--- server.js
+++ server.js
@@ -13,11 +13,51 @@ const MOCKSDIRECTORY = 'mocks';
 const app = express();
 app.use(express.json());
 
-fs.readdir(MOCKSDIRECTORY, (err, files) => {
+fs.readdir(MOCKSDIRECTORY + '/get', (err, files) => {
   files.forEach(file => {
-    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/' + file);
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/get/' + file);
 
     app.get('/' + path.parse(file).name, (req, res) => {
+      res.json(JSON.parse(mockData))
+    });
+  });
+});
+
+fs.readdir(MOCKSDIRECTORY + '/post', (err, files) => {
+  files.forEach(file => {
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/post/' + file);
+
+    app.post('/' + path.parse(file).name, (req, res) => {
+      res.json(JSON.parse(mockData))
+    });
+  });
+});
+
+fs.readdir(MOCKSDIRECTORY + '/delete', (err, files) => {
+  files.forEach(file => {
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/delete/' + file);
+
+    app.delete('/' + path.parse(file).name, (req, res) => {
+      res.json(JSON.parse(mockData))
+    });
+  });
+});
+
+fs.readdir(MOCKSDIRECTORY + '/put', (err, files) => {
+  files.forEach(file => {
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/put/' + file);
+
+    app.put('/' + path.parse(file).name, (req, res) => {
+      res.json(JSON.parse(mockData))
+    });
+  });
+});
+
+fs.readdir(MOCKSDIRECTORY + '/patch', (err, files) => {
+  files.forEach(file => {
+    let mockData = fs.readFileSync(MOCKSDIRECTORY + '/patch/' + file);
+
+    app.patch('/' + path.parse(file).name, (req, res) => {
       res.json(JSON.parse(mockData))
     });
   });
